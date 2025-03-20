8220de1dd04229f435db837c7d79721a44521b63
dishabehere@gmail.com <dishabehere@gmail.com>
2025-03-20 17:46:09 -0400

    Add the tablet and desktop styling.

--- bookhive/src/pages/LandingPage/LandingPage.jsx
+++ bookhive/src/pages/LandingPage/LandingPage.jsx
@@ -6,17 +6,17 @@ function LandingPage() {
     return(
         <section className="landingpage">
             <img className="landingpage__hero" src={hero} alt="Hero Image of a Library" />
+            <h1 className="landingpage__title">Welcome to BookHive🐝!</h1>
             <div className="landingpage__container">
                 <div className="landingpage__content">
-                    <h1 className="landingpage__title">Welcome to BookHive🐝!</h1>
                     <p className="landingpage__text">Find your next favourite read without breaking the bank. Rent books from others in your area, or share your own collection and earn some extra cash. It's sustainable, affordable, and fun!</p>
                 </div>
                 <div className="landingpage__info">
                     <p className="landingpage__text">Discover new stories and connect with fellow bookworms in your community. Join the hive today!</p>
                     <div className="landingpage__buttons">
-                        <button className="landingpage__button landingpage__button--login">LogIn</button>
+                        <button className="landingpage__button landingpage__button--login"><h3>LogIn</h3></button>
                         <p className="landingpage__text landingpage__text--buttons">OR</p>
-                        <button className="landingpage__button landingpage__button--signup">SignUp</button>
+                        <button className="landingpage__button landingpage__button--signup"><h3>SignUp</h3></button>
                     </div>
                 </div>
             </div>
