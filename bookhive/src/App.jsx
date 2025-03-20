9e7e1d0572e52f7d4af56b0aa1cf0444943722c2
dishabehere@gmail.com <dishabehere@gmail.com>
2025-03-20 14:19:36 -0400

    Create the Footer.jsx file

--- bookhive/src/App.jsx
+++ bookhive/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "../src/components/Header/Header.jsx";
+import Footer from "../src/components/Footer/Footer.jsx";
+import LandingPage from "../src/pages/LandingPage/LandingPage.jsx";
 import './App.scss';
 
 function App() {
@@ -7,9 +9,9 @@ function App() {
     <Router>
       <Header />
       <Routes>
-      {/* <Route path="/" element={<LandingPage />} /> */}
+      <Route path="/" element={<LandingPage />} />
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
     </Router>
     
   );
