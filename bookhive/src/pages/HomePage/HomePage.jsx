fe531b9e37b274d2d2d5efe52d10928ab3b973c9
dishabehere@gmail.com <dishabehere@gmail.com>
2025-03-20 22:02:49 -0400

    Create the Home Page jsx file.

--- bookhive/src/pages/HomePage/HomePage.jsx
+++ bookhive/src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import searchIcon from "../../assets/icons/search-24px.svg";
 import './HomePage.scss';
 
 function HomePage() {
     return(
-        <section className="homepage">
-            
+        <section className="home">
+            <section className="home__header">
+                <h1 className="home__title">Rent Books</h1>
+                <div className="warehouse-list__search-bar">
+                    <input
+                    className="warehouse-list__search-text"
+                    type="text"
+                    placeholder="Search..."
+                    />
+                    <img
+                    className="warehouse-list__search-icon"
+                    src={searchIcon}
+                    alt="Search"
+                    />
+                </div>
+            </section>
+            <section className="home__books">
+                <h2 className="home__sub-heading">Available Books</h2>
+            </section>
         </section>
     );
 }
