dc6892632ff1007dc7ab6cfb06fcf28fb00aab72
dishabehere@gmail.com <dishabehere@gmail.com>
2025-03-23 11:28:47 -0400

    Adjust the UI

--- bookhive/src/pages/MyBooksPage/MyBooksPage.jsx
+++ bookhive/src/pages/MyBooksPage/MyBooksPage.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams, useLocation } from "react-router-dom";
 import { getMyBooks, getRentalBooks } from "../../utils/apiUtils.jsx";
+import { Link } from "react-router-dom";
+import arrow from "../../assets/icons/arrow_back-24px.svg";
 import BooksGrid from "../../components/BooksGrid/BooksGrid";
 import "./MyBooksPage.scss";
 
@@ -8,6 +10,7 @@ function MyBooksPage() {
     // const {user_id} = useParams(); // Ideally it should get user ID from the route
     const location = useLocation();
     const [books, setBooks] = useState([]);
+    const isMyRentalsPage = location.pathname.includes("/rentalBooks");
     
     const user_id = 20; // Temporary hardcoded user ID
 
@@ -32,7 +35,14 @@ function MyBooksPage() {
     return (
         <section className="mybooks">
             <section className="mybooks__header">
-                
+                <Link to={`/Home`} className="mybooks__header-link">
+                    <img className="mybooks__arrow-icon" src={arrow} alt="Backwards Arrow" />
+                </Link>
+                {isMyRentalsPage ? (
+                    <h1 className="mybooks__rental-books">My Rentals</h1>
+                    ) : (
+                    <h1 className="mybooks__my-books">My Listings</h1>
+                )}
             </section>
             <BooksGrid books={books} />
         </section>
