45cf88ca033b58b421a000ff53fbb750b0e48997
Jonathan Mazin <jmazin@brainstation.io>
2025-03-26 16:15:13 -0400

    some style updates

--- bookhive/src/components/BooksGrid/BooksGrid.jsx
+++ bookhive/src/components/BooksGrid/BooksGrid.jsx
@@ -6,7 +6,7 @@ function BooksGrid({ books, isMyBooksPage, isMyRentalsPage, showEditOptions }) {
   return (
     <div className={`books-grid ${isMyBooksPage ? "books-grid--mybooks" : ""} ${isMyRentalsPage ? "books-grid--mybooks" : ""}`}>
       {books.map((book, index) => (
-        <div key={book.id} className={`book-container ${index % 2 !== 0 ? "shifted" : ""} ${index % 3 !== 0 ? "shift" : ""}`}>
+        <div key={book.id} className={`book-container ${index % 2 !== 0 ? "shifted" : ""}`}>
           <Book book={book} showEditOptions={showEditOptions} fromMyBooksPage={isMyBooksPage} fromMyRentalsPage={isMyRentalsPage}/>
         </div>
       ))}
