c1cc83ae99c5690a830436d57990de62a6f3f9d8
Jonathan Mazin <jmazin@brainstation.io>
2025-03-26 17:08:54 -0400

    update styles

--- bookhive/src/components/BooksGrid/BooksGrid.jsx
+++ bookhive/src/components/BooksGrid/BooksGrid.jsx
@@ -4,12 +4,19 @@ import "./BooksGrid.scss";
 
 function BooksGrid({ books, isMyBooksPage, isMyRentalsPage, showEditOptions }) {
   return (
-    <div className={`books-grid ${isMyBooksPage ? "books-grid--mybooks" : ""} ${isMyRentalsPage ? "books-grid--mybooks" : ""}`}>
-      {books.map((book, index) => (
-        <div key={book.id} className={`book-container ${index % 2 !== 0 ? "shifted" : ""}`}>
-          <Book book={book} showEditOptions={showEditOptions} fromMyBooksPage={isMyBooksPage} fromMyRentalsPage={isMyRentalsPage}/>
-        </div>
-      ))}
+    <div className={`books-grid ${isMyBooksPage || isMyRentalsPage ? "books-grid--mybooks" : ""} `}>
+      {books.map((book) => {
+        return (
+          <div key={book.id} className={`book-container`}>
+            <Book
+              book={book}
+              showEditOptions={showEditOptions}
+              fromMyBooksPage={isMyBooksPage}
+              fromMyRentalsPage={isMyRentalsPage}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 }
